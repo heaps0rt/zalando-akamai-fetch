db01e6e9c3f5350609cd0b36e1694798464d273d
Raviksan Navaratnarajah <ravi@Raviksans-MacBook-Pro.local>
2022-10-09 20:43:20 +0200

    rewrote everything

--- main.go
+++ main.go
@@ -3,35 +3,65 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
-var url string
+var url = "https://zalando.no"
 
-func fetchParse() {
-	fmt.Println("What website would you like to target?")
-	fmt.Scanln(&url)
-	resp, err := http.Get("https://" + url)
+func FetchParse(url string) []string {
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
 
 	defer resp.Body.Close()
 
+	/*
+		html, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s", html)
+	*/
+
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	var uri []string
 	doc.Find("script").Each(func(i int, s *goquery.Selection) {
 		if typee, _ := s.Attr("type"); typee == "text/javascript" {
 			textjavascript, _ := s.Attr("src")
-			fmt.Printf("https://%s%s\n", url, textjavascript)
+			uri = append(uri, textjavascript)
 		}
 	})
+
+	var fullURL []string
+	for _, uri := range uri {
+		fullURL := url + uri
+		response, err := http.Get(fullURL)
+		if err != nil {
+			panic(err)
+		}
+
+		html, err := io.ReadAll(response.Body)
+		if err != nil {
+			panic(err)
+		}
+		if strings.Contains(string(html), "bmak") == true {
+			fmt.Println(fullURL)
+			break
+		} else {
+			continue
+		}
+	}
+	return fullURL
 }
 
 func main() {
-	fetchParse()
+	FetchParse(url)
 }
